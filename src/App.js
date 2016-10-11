6b3650cc63b02a28ae6aaaceb0641da39542f748
Karthik <kartu8@gmail.com>
2016-10-11 14:39:21 +0530

    added default text to preview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Remarkable from 'remarkable'
-import './App.css';
+import './App.css'
 
 class Markdown extends Component {
     constructor() {
@@ -61,8 +61,43 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.handleChange = this.handleChange.bind(this)
+        const defaultText = `
+Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a
+line break
+
+Text attributes *italic*, **bold**,
+\`monospace\`, ~~strikethrough~~ .
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in
+Spain.
+
+*[Herman Fassett](https://freecodecamp.com/hermanfassett)*
+        `
         this.state = {
-            text: 'hello'
+            text: defaultText
         }
     }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import './index.css'
 
 render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
